d95bffbb2d0e8bd44669be47875b5e6a889b2f0d
HanSeongJae <fhqhxm8899@gmail.com>
2022-07-31 23:40:13 +0900

    database schema dev로 변경

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE_TEST,
+  database: process.env.DB_DATABASE_DEV,
 });
 
 pool.on("connection", (connection) => {
