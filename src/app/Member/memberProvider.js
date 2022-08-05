5f4d8d2a524f486ce29ae1113ebb8ada204620bd
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-08-05 17:04:44 +0900

    feature/member API 3.1, 3.3

--- src/app/Member/memberProvider.js
+++ src/app/Member/memberProvider.js
@@ -5,6 +5,7 @@ const { logger } = require("../../../config/winston");
 
 const memberDao = require("./memberDao");
 
+// DB Test
 exports.retrieveUserList = async function () {
     const connection = await pool.getConnection(async (conn) => conn);
     const handleError = (error) => logger.error(`❌DB Error: ${error.message}`);
@@ -19,3 +20,75 @@ exports.retrieveUserList = async function () {
       return errResponse(baseResponseStatus.FAILURE);
     }
   };
+
+// 단체 모든 회원명단 리스트 조회 - API NO. 3.1
+exports.retrieveClubMemberList = async function (adminIdx) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const handleError = (error) => logger.error(`❌DB Error: ${error.message}`);
+
+  //Try문 예외처리
+  try {
+    const clubMemberResult = await memberDao.selectClub(connection, adminIdx);
+    connection.release();
+    return clubMemberResult;
+
+  } catch (error) {
+    handleError(error);
+    connection.release();
+    return errResponse(baseResponseStatus.FAILURE);
+  }
+};
+
+// API NO. 3.1 - AdminIdx Status Check
+exports.checkClubStatus = async function (adminIdx) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const handleError = (error) => logger.error(`❌DB Error: ${error.message}`);
+
+  //Try문 예외처리
+  try {
+    const clubStatus = await memberDao.selectClubStatus(connection, adminIdx);
+    connection.release();
+    return clubStatus[0].status;
+
+  } catch (error) {
+    handleError(error);
+    connection.release();
+    return errResponse(baseResponseStatus.FAILURE);
+  }
+};
+
+// 회원 상세 조회 - API NO. 3.3
+exports.retrieveMemberInfo = async function (userIdx) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const handleError = (error) => logger.error(`❌DB Error: ${error.message}`);
+
+  //Try문 예외처리
+  try {
+    const memberInfo = await memberDao.selectMemberInfo(connection, userIdx);
+    connection.release();
+    return memberInfo;
+
+  } catch (error) {
+    handleError(error);
+    connection.release();
+    return errResponse(baseResponseStatus.FAILURE);
+  }
+};
+
+// API NO. 3.3 - User Status Check
+exports.checkUserStatus = async function (userIdx) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const handleError = (error) => logger.error(`❌DB Error: ${error.message}`);
+
+  //Try문 예외처리
+  try {
+    const userStatus = await memberDao.selectUserStatus(connection, userIdx);
+    connection.release();
+    return userStatus[0].status;
+
+  } catch (error) {
+    handleError(error);
+    connection.release();
+    return errResponse(baseResponseStatus.FAILURE);
+  }
+};
