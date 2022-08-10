bdf251bdb562270765a9fed72afc8d1426739d88
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-08-11 01:04:27 +0900

    ✅ AWS EC2 PortListening

--- init.js
+++ init.js
@@ -2,9 +2,19 @@ const app = require("./config/express");
 import "dotenv/config";
 const { logger } = require("./config/winston");
 
+// AWS EC2 포트리스닝
+const http = require('http');
+const hostname = '3.38.55.57';
 let PORTNUM = 3000;
 
-app().listen(PORTNUM, () => {
+const server = http.createServer((req, res) => {
+  res.statusCode = 2000;
+  res.setHeader('동네 백엔드');
+  res.end('동네 백엔드 개발자 화이팅!\n');
+});
+
+
+server.listen(PORTNUM,hostname,() => {
   logger.info(`✅Start Express Server on port ${PORTNUM} `);
-  console.log(`✅ Check it out! at here --> http://localhost:${PORTNUM}/`);
+  console.log(`✅ Check it out! at here --> http://${hostname}:${PORTNUM}/`);
 });
