ae412a57ea66af3eb0220688e740b0f7880a32e0
HanSeongJae <fhqhxm8899@gmail.com>
2022-08-15 00:28:23 +0900

    :white_check_mark: Swagger: user/auth

--- modules/swagger.js
+++ modules/swagger.js
@@ -13,7 +13,7 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition: swaggerDefinition,
-  apis: ["./src/admin/Testinit/*.js", "./src/admin/Attendance/*.js", "./src/admin/Schedule/*.js", "./src/admin/Group/*.js", "./src/admin/Member/*.js", "./src/admin/Auth/*.js"]
+  apis: ["./src/admin/Testinit/*.js", "./src/admin/Attendance/*.js", "./src/admin/Schedule/*.js", "./src/admin/Group/*.js", "./src/admin/Member/*.js", "./src/admin/Auth/*.js", "./src/user/Auth/*.js"]
 };
 
 const specs = swaggerJsdoc(options);
